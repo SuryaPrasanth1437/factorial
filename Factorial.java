7e8b041912053b867ff3a1691949c43a15dff5a6
SuryaPrasanth1437 <37958227+SuryaPrasanth1437@users.noreply.github.com>
2018-08-08 13:51:39 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package program;
+
+public class Factorial
+{
+
+	public static void main(String[] args)
+	{
+		int i,fact=1;
+		for (i=1;i<=10;i++)
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println("the factorial of the first number is"+fact);
+	}
+
+}
